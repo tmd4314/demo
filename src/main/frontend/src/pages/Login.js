270b2dbc4eba8928eabd930e446879f8b79b5733
bong <p1-02@naver.com>
2024-05-02 11:29:39 +0900

    Join.js, Login.js -> Join, Login fix

--- src/main/frontend/src/pages/Login.js
+++ src/main/frontend/src/pages/Login.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import '../css/Login.css';
 
 function Login() {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>ID</Form.Label>
-                <Form.Control type="username" placeholder="Enter ID" />
-                <Form.Text className="text-muted">
-                    아이디를 작성해주세요.
-                </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                로그인
-            </Button>
-        </Form>
+        <div className='login-box'>
+            <h3>로그인</h3>
+            <div>
+                <div className="form-item">
+                    <Form.Control type="username" placeholder="아이디" />
+                </div>
+                <div className="form-item">
+                    <Form.Control type="password" placeholder="비밀번호" />
+                </div>  
+                <div className="button-container">
+                    <Button variant="primary" type="submit" id='loginBtn'>로그인</Button>
+                    <a href='/join'>
+                        <Button variant="secondary" id='joinBtn'>회원가입</Button>
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 }
 
